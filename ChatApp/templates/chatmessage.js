c8f3caad3c65d817acd891cd8bbfda6142962d7f
keisukeKU <ku.raretech.signup@gmail.com>
2025-05-29 15:28:00 +0900

    #

--- ChatApp/templates/chatmessage.js
+++ ChatApp/templates/chatmessage.js
@@ -7,6 +7,8 @@ const chatInputForm = document.querySelector('.chat-input-form') //30行目
 const chatInput = document.querySelector('.chat-input') //31行目
 const clearChatBtn = document.querySelector('.clear-chat-button') //34行目
 
+const messages = JSON.parse(localStorage.getItem('messages')) || []
+
 // •document：今開いている HTMLページ全体
 // •querySelector('#'または’.')：その中から、id が user-selector の要素を1つ取得
 // •const userSelectorBtn：取得した要素を userSelectorBtn という変数に入れて記憶
@@ -32,6 +34,12 @@ const createChatMessageElement = (message) => `
 // --------------------------------------------------------------------------------------------------------------------\\
 
 // 3.メッセージ送信関数を作成
+
+window.onload = () => {
+    messages.forEach((message) => {
+        chatMessages.innerHTML += createChatMessageElement(message)
+  })
+}
 
 //チャットしているユーザーの表示切り替え(<h2>)
 let messageSender = 'user'
@@ -70,6 +78,8 @@ user2SelectorBtn.onclick = () => updateMessageSender('user2')
        text: chatInput.value,
        timestamp,
 
+
+
     //1.new Date()　現在の日時を表す JavaScript の Date オブジェクトを生成します。
     //2.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })生成した Date オブジェクトを、指定したロケール（ここではアメリカ英語）とオプションで文字列に変換します。
     //3.const timestamp = … 上記で得られた時刻文字列を変数 timestamp に格納します。
@@ -77,7 +87,10 @@ user2SelectorBtn.onclick = () => updateMessageSender('user2')
     //5.後続処理へ）こうして作成した message オブジェクトを、画面への表示やサーバ送信など、チャット機能の次のステップに渡して使います。
     }
 
-    chatMessages.innerHTML += createChatMessageElement(message)
+        messages.push(message)
+        localStorage.setItem('messages', JSON.stringify(messages))
+        chatMessages.innerHTML += createChatMessageElement(message)
+
 
     // チャットを送信すると最新のメッセージを表示してくれる
     chatInputForm.reset()
@@ -85,3 +98,9 @@ user2SelectorBtn.onclick = () => updateMessageSender('user2')
 }
 
 chatInputForm.addEventListener('submit', sendMessage)
+
+clearChatBtn.addEventListener('click',() => {
+    localStorage.clear()
+    chatMessages.innerHTML = ''
+})
+    
